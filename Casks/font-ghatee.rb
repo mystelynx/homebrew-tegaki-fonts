0780b97d9a850919c9c8e5ed28ed0bea69dba9fc
mystelynx <t@urau.la>
2015-11-04 02:15:56 +0900

    change otf to ttf

--- Casks/font-ghatee.rb
+++ Casks/font-ghatee.rb
@@ -2,10 +2,10 @@ cask :v1 => 'font-ghatee' do
   version '2.06'
   sha256 :no_check
 
-  url 'http://marusexijaxs.web.fc2.com/ghatee_ote.zip'
+  url 'http://marusexijaxs.web.fc2.com/ghatee_tte.zip'
   homepage 'http://marusexijaxs.web.fc2.com/tegakifont.html#tegakifont2'
   license :unknown
 
-  font 'ghatee.otf'
+  font 'ghatee.ttf'
 end
 
