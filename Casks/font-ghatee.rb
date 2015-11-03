9e407054c6f47bb8ef4023579b878bce52b40ee7
mystelynx <t@urau.la>
2015-11-04 02:11:59 +0900

    add ghatee

---
+++ Casks/font-ghatee.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-ghatee' do
+  version '2.06'
+  sha256 :no_check
+
+  url 'http://marusexijaxs.web.fc2.com/ghatee_ote.zip'
+  homepage 'http://marusexijaxs.web.fc2.com/tegakifont.html#tegakifont2'
+  license :unknown
+
+  font 'ghatee.otf'
+end
+
