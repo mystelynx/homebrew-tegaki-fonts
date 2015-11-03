f30fb5859948a5bf83aa12202a07c41dd2a71557
mystelynx <t@urau.la>
2015-11-04 01:51:13 +0900

    add 851tegaki_zatsu formula

---
+++ Formula/851tegaki_zatsu.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-851tegaki_zatsu' do
+  version '0.880'
+  sha256 :no_check
+
+  url 'https://dl.dropboxusercontent.com/u/77785050/851tegaki_zatsu.ttf'
+  homepage 'http://www39.atpages.jp/yagoinienie/851fontpage.html'
+  license :ofl
+
+  font '851tegaki_zatsu.ttf'
+end
