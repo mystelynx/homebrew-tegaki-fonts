f7a3d9ec1079a4574ea3d695aac99efa772d0542
mystelynx <t@urau.la>
2015-11-04 03:27:24 +0900

    add 851tegaki_zatsu_bold formula

---
+++ Casks/font-851tegaki_zatsu_bold.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-851tegaki_zatsu_bold' do
+  version '0.875'
+  sha256 :no_check
+
+  url 'https://dl.dropboxusercontent.com/u/77785050/851tegakizatsu_0875_bold.ttf'
+  homepage 'http://www39.atpages.jp/yagoinienie/851fontpage.html'
+  license :unknown
+
+  font '851tegakizatsu_bold.ttf'
+end
+
