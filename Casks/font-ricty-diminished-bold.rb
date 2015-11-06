d989080a83a1327035b7bfef4c188d5d0ae9d9a0
mystelynx <t@urau.la>
2015-11-06 12:25:02 +0900

    add RictyDiminished series

---
+++ Casks/font-ricty-diminished-bold.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-ricty-diminished-bold' do
+  version '3.2.4'
+  sha256 :no_check
+
+  url 'https://github.com/yascentur/RictyDiminished/raw/0d2e2820808c61133de1e7bede68e69e556481ec/RictyDiminished-Bold.ttf'
+  homepage 'https://github.com/yascentur/RictyDiminished'
+  license :ofl
+
+  font 'RictyDiminished-Bold.ttf'
+end
+
