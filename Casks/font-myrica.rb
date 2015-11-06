e1c27789a04738455fc9121b6abc59f69de6cd13
mystelynx <t@urau.la>
2015-11-06 11:57:03 +0900

    add myrica

---
+++ Casks/font-myrica.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-myrica' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip'
+  homepage 'http://myrica.estable.jp/'
+  license :unknown
+
+  font 'Myrica/Myrica.TTC'
+end
+
